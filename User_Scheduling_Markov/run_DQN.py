52825c24f84884fa3990984dc98b8aa4b04b135b
Nimrod <nimrod1988@gmail.com>
2019-12-13 02:30:42 -0500

    Added avg to test

--- User_Scheduling_Markov/run_DQN.py
+++ User_Scheduling_Markov/run_DQN.py
@@ -55,39 +55,54 @@ def update():
 def test():
     global state_action
     global start_state
-    start_state = env.create_rayleigh_fading()
-    observation = env.reset(start_state)
-    timer_tti = 0
     RL.load_model()
 
-    for episode in range(max_test):
-        print("test " + str(episode))
+    for iter in range(0,10):
+        start_state = env.create_rayleigh_fading()
+        observation = env.reset(start_state)
+        timer_tti = 0
+        for episode in range(max_test):
+            string = "Log_Thr_1000_tti_epsilon_decay_100000000_NN_SU_3_ues_no_max_tti_riti_same_rayleigh" + str(iter) + ".csv"
 
-        # RL choose action based on observation
-        action = RL.choose_action_test(observation)
+            print("test " + str(episode))
 
-        # RL take action and get next observation and reward
-        observation_ = env.step_test(action, observation, start_state, episode, timer_tti)
+            # RL choose action based on observation
+            action = RL.choose_action_test(observation)
 
-        # swap observation
-        observation = observation_
+            # RL take action and get next observation and reward
+            observation_ = env.step_test(action, observation, start_state, episode, timer_tti)
 
-        timer_tti += 1
-        timer_tti = timer_tti % env.time_window_test
+            # swap observation
+            observation = observation_
 
-    print('testing over')
-    with open("Log_Thr_1000_tti_epsilon_decay_60000000_NN_SU_no_max_tti_riti.csv", "a") as thr:
-        thr_csv = csv.writer(thr, dialect='excel')
-        thr_csv.writerow(enviroment_DQN.diff)
-        thr.close()
+            timer_tti += 1
+            timer_tti = timer_tti % env.time_window_test
+
+        print('testing ' + str(iter) + ' over')
+        with open(string, "a") as thr:
+            thr_csv = csv.writer(thr, dialect='excel')
+            thr_csv.writerow(enviroment_DQN.diff)
+            thr.close()
+        enviroment_DQN.diff = []
 
 
 
 def plot():
-    data = np.genfromtxt('Log_Thr_1000_tti_epsilon_decay_60000000_NN_SU_no_max_tti_riti.csv',
-                         delimiter=',')
+
+
+    iter = 0
+    string = "Log_Thr_1000_tti_epsilon_decay_100000000_NN_SU_3_ues_no_max_tti_riti_same_rayleigh" + str(iter) + ".csv"
+    data = np.genfromtxt(string, delimiter=',')
+    data2d_1 = np.atleast_2d(data)
+    for iter in range(1, 10):
+        string = "Log_Thr_1000_tti_epsilon_decay_100000000_NN_SU_3_ues_no_max_tti_riti_same_rayleigh" + str(iter) + ".csv"
+        data = np.genfromtxt(string, delimiter=',')
+        data2d = np.atleast_2d(data)
+        gather = np.concatenate((data2d_1, data2d), axis=0)
+        data2d_1 = gather
+    mean_result = np.mean(data2d_1, axis=0)
     sns.set(color_codes=True)
-    sns.distplot(data, kde=False)
+    sns.distplot(mean_result, kde=False)
     plt.xlabel("LogThr BF vs RL")
     plt.ylabel("Number of Observations")
     plt.show()
@@ -122,8 +137,8 @@ if __name__ == "__main__":
                       # output_graph=True
                       )
 
-    update()
-    #test()
+    #update()
+    test()
     #with open("Log_Thr_2_tti_test_0.9_epsilon_decay_60000000_NN_SU.csv", "a") as thr:
         #thr_csv = csv.writer(thr, dialect='excel')
         #thr_csv.writerow(enviroment_DQN.diff)
